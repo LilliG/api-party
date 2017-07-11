4ebd500e445af480761d16f4f120d85961e939b3
Lillian Gray <lmgray16@earlham.edu>
2017-07-11 15:16:40 -0400

    Add markup and styling for Github component

---
+++ src/Github.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './Github.css'
+
+class Github extends Component {
+    render() {
+        return (
+            <div className='github'>
+                <img 
+                    className="github-logo"
+                    src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" 
+                    alt="github logo"
+                />
+                <form>
+                    <div>
+                        
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Github
