b68c5d3a574085d2ac2aaeddbca03c89c62e421a
Lillian Gray <lmgray16@earlham.edu>
2017-07-12 12:55:44 -0400

    WIP: Add pokemon API

--- src/Pokéapi.js
+++ src/Pokéapi.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 import './Pokéapi.css'
 import { Route } from 'react-router-dom'
-// import Pokémon from './Pokémon'
+import Pokémon from './Pokémon'
 
 class Pokéapi extends Component {
     state = {
-        pokémon: ''
+        pokémonName: ''
     }
 
     handleChange = (ev) => {
-        this.setState({ pokémon: ev.target.value })
+        this.setState({ pokémonName: ev.target.value })
     }
 
     handleSubmit = (ev) => {
         ev.preventDefault()
-        this.props.history.push(`/pokéapi/${this.state.pokémon}`)
-        this.setState({ pokémon: '' })
+        this.props.history.push(`/pokéapi/${this.state.pokémonName}`)
+        this.setState({ pokémonName: '' })
     }
 
     render() {
@@ -26,14 +26,14 @@ class Pokéapi extends Component {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/2000px-Pok%C3%A9_Ball_icon.svg.png"
             alt="pokéball"
             />
-            <h1>
+            <h1 className="pokéapi-heading">
                 Pokéapi
             </h1>
             <form onSubmit={this.handleSubmit}>
             <div>
                 <input
                     type="text"
-                    value={this.state.pokémon}
+                    value={this.state.pokémonName}
                     onChange={this.handleChange}
                 />
             </div>
@@ -41,7 +41,8 @@ class Pokéapi extends Component {
                 <button type="submit">Look up Pokémon</button>
             </div>
             </form>
-            {/*<Route path="/pokéapi/:pokémon" component={Pokémon} />*/}
+            <Route path="/pokéapi/:pokemonName" component={Pokémon} />
+            {/*<Route path="/pokéapi/:pokemonName" render={(props) => <h3>You searched for {props.match.params.pokemonName}</h3>} />*/}
             <Route exact path="/pokéapi" render={() => <h3>Please enter a Pokémon to search on Pokéapi.</h3>} />
         </div>
         )
